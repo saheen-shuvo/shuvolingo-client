3d517e560bae26eed9c70702dbac4b1fd980d16d
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-01-24 22:03:48 +0600

    Register added

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import './App.css'
 import router from './router/router.jsx'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './context/AuthContext/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <AuthProvider>
      <RouterProvider router={router} />
+     </AuthProvider>
   </StrictMode>,
 )
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,91 @@
+import Lottie from "lottie-react";
+import registerLottieData from "../../assets/lottie/register.json";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext/AuthContext";
+// import SocialLogin from "../shared/SocialLogin";
 
 const Register = () => {
-    return (
-        <div>
-            
+  const { createUser } = useContext(AuthContext);
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    // Password Validation
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/;
+
+    if (!passwordRegex.test(password)) {
+      alert(
+        "Password must be at least 6 characters long, include an uppercase letter, a lowercase letter, and a number."
+      );
+      return;
+    }
+
+    form.reset();
+
+        createUser(email, password)
+          .then((result) => {
+            console.log(result.user);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left w-96">
+          <Lottie animationData={registerLottieData}></Lottie>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className=" mx-auto mt-3 text-5xl font-bold">Register now!</h1>
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+          <div className="divider">OR</div>
+          <div className="flex justify-center">
+            {/* <SocialLogin></SocialLogin> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
